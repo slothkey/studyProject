c3d91b3718c75e6a594d86d6bd76188a20126a36
hn <717578983@qq.com>
2018-03-26 16:26:54 +0800

    手写签名（之后可以看下源码，熟悉贝赛尔曲线的使用）

---
+++ app/src/main/java/com/heyi/demo/signature/SignatureActivity.java
@@ -0,0 +1,17 @@
+package com.heyi.demo.signature;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.heyi.demo.R;
+
+// 手写签名
+public class SignatureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signature);
+    }
+}
